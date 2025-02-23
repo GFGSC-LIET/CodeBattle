745389adcf181e0ada634275768bdeebc0c0b0e7
TejaswaniVemula <144512754+TejaswaniVemula@users.noreply.github.com>
2025-02-23 13:05:02 +0530

    Create Solution_CPP.cpp

---
+++ Calculate_the_Cost_of_Fresh_Items/Solution_CPP.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,sum=0;
+        cin>>n>>x;
+        int a[n],b[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int j=0;j<n;j++){
+            cin>>b[j];
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]>=x){
+                sum+=b[i];
+            }
+        }
+        cout<<sum<<endl;
+    }
+
+}
