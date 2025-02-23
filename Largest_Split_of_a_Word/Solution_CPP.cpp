42b37a87a923bd81a90d41f30d82ac7f907337e0
TejaswaniVemula <144512754+TejaswaniVemula@users.noreply.github.com>
2025-02-23 13:29:22 +0530

    Create Solution_CPP.cpp

---
+++ Largest_Split_of_a_Word/Solution_CPP.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+string answerString(string word, int n) {
+    if (n == 1) return word;
+    int max_len = word.length() - n + 1;
+    char max_char = word[0];
+    for (auto i = 1; i < word.length(); i++) {
+        if (word[i] > max_char) {
+            max_char = word[i];
+        }
+    }
+    string my_ans = " ";
+    for (auto i = 0; i < word.length(); i++) {
+        if (word[i] == max_char) {
+            my_ans = max(my_ans, word.substr(i, max_len));
+        }
+    }
+    return my_ans;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string word;
+        int n;
+        cin >> word;
+        cin >> n;
+        string ans = answerString(word, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
