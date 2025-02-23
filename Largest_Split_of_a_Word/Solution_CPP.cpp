582697789caf61132331286c3fb4de8137df4d46
Maradana Jeevana <152767489+Jeevana52@users.noreply.github.com>
2025-02-23 17:03:00 +0530

    Update Solution_CPP.cpp

--- Largest_Split_of_a_Word/Solution_CPP.cpp
+++ Largest_Split_of_a_Word/Solution_CPP.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 string answerString(string word, int n) {
     if (n == 1) return word;
-    int max_len = word.length() - n + 1;
+    size_t max_len = word.length() - n + 1;
     char max_char = word[0];
-    for (auto i = 1; i < word.length(); i++) {
+    for (size_t i = 1; i < word.length(); i++) {
         if (word[i] > max_char) {
             max_char = word[i];
         }
     }
     string my_ans = " ";
-    for (auto i = 0; i < word.length(); i++) {
+    for (size_t i = 0; i < word.length(); i++) {
         if (word[i] == max_char) {
             my_ans = max(my_ans, word.substr(i, max_len));
         }
